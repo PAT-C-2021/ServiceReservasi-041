afc5fc69eb5364221219643141de5af798da9490
Nando Alhasanain <Nandoalhasanain@gmail.com>
2021-12-13 15:51:34 +0700

    Praktikum 9

--- ServiceReservasi/IService1.cs
+++ ServiceReservasi/IService1.cs
@@ -13,7 +13,7 @@ namespace ServiceReservasi
         [OperationContract]
         string pemesanan(string IDPemesanan, string NamaCustomer, string NoTelpon, int JumlahPemesanan, string IDLokasi);
         [OperationContract]
-        string editPemesanan(string IDPemesanan, string NamaCustomer);
+        string editPemesanan(string IDPemesanan, string NamaCustomer, string No_Telpon);
         [OperationContract]
         string deletePemesanan(string IDPemesanan);
         [OperationContract]
@@ -61,7 +61,7 @@ namespace ServiceReservasi
         [DataMember]
         public int JumlahPemesanan { get; set; }
         [DataMember]
-        public string IDLokasi { get; set; }
+        public string Lokasi { get; set; }
     }
 
 
